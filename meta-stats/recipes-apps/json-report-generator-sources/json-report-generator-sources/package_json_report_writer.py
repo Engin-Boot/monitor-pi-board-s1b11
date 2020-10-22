a9abf598edaff77ae4ac09ac4f530a14cd577999
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-10-22 12:45:36 +0530

    Update package_json_report_writer.py

--- meta-stats/recipes-apps/json-report-generator-sources/json-report-generator-sources/package_json_report_writer.py
+++ meta-stats/recipes-apps/json-report-generator-sources/json-report-generator-sources/package_json_report_writer.py
@@ -1,9 +1,27 @@
+'''
+Writes JSON object to a JSON file.
+
+Function:
+
+    write_json_report(str, str)
+'''
+
 import os
 
-def write_json_report(output_directory, package_json_object):
-	# create output JSON file path
-	json_file = os.path.join(output_directory, 'package-json-report.json')
 
-	with open(json_file, "w") as package_json_report:
-		package_json_report.write(package_json_object)
-	print("JSON report creation successful")
+def write_json_report(output_directory, package_json_object):
+    '''
+    Writes JSON object to a JSON file.
+
+    Parameters:
+
+        output_directory (str): output directory path
+        package_json_object (str): JSON string
+    '''
+
+    # create output JSON file path
+    json_file = os.path.join(output_directory, 'package-json-report.json')
+
+    with open(json_file, "w") as package_json_report:
+        package_json_report.write(package_json_object)
+    print("JSON report creation successful")
