a65d4350dce42c278b1b1f3b99d4bb8e7581e9d0
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-10-22 12:43:18 +0530

    Update json-report-generator.py

--- meta-stats/recipes-apps/json-report-generator-sources/json-report-generator-sources/json-report-generator.py
+++ meta-stats/recipes-apps/json-report-generator-sources/json-report-generator-sources/json-report-generator.py
@@ -1,3 +1,12 @@
+'''
+Main source file
+
+Imports:
+
+    package_json_object_creator
+    package_json_report_writer
+'''
+
 import argparse
 import os
 import sys
@@ -5,25 +14,29 @@ import package_json_object_creator
 import package_json_report_writer
 
 # parse the command line arguments
-parser = argparse.ArgumentParser(description='manifest file to JSON file convertor')
-parser.add_argument('-i', '--input', help='absolute path to input manifest file in .manifest format', type=os.path.abspath, required=True)
-parser.add_argument('-o', '--output', help='absolute path to output directory', type=os.path.abspath, required=True)
+parser = argparse.ArgumentParser(description='manifest file \
+                                to JSON file convertor')
+parser.add_argument('-i', '--input', help='absolute path \
+                   to input manifest file in .manifest format',
+                    type=os.path.abspath, required=True)
+parser.add_argument('-o', '--output', help='absolute path to \
+                   output directory', type=os.path.abspath, required=True)
 args = parser.parse_args()
 
-print("Received arguments, input manifest file path: " + args.input + " , output directory: " + args.output)
+print("Received arguments, input manifest file path: "
+      + args.input + " , output directory: " + args.output)
 
 # check if input manifest file exists
 input_manifest_file = args.input
 if not os.path.exists(input_manifest_file):
-	print("ERROR: input manifest file not found!")
-	sys.exit(1)
-	
+    print("ERROR: input manifest file not found!")
+    sys.exit(1)
 
 # check if output directory exists
 output_directory = args.output
 if not os.path.exists(output_directory):
-	print("ERROR: output directory not found!")
-	sys.exit(1)
+    print("ERROR: output directory not found!")
+    sys.exit(1)
 
 # create package JSON object
 package_dict = package_json_object_creator.create_package_json_object(input_manifest_file)
